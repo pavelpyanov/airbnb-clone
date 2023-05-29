c04d5f0f4f8f6512d257c38d07f7b0e1da81c297
Pyanov, Pavel <pavel.pyanov@x5.ru>
2023-05-29 16:49:37 +0300

    Add cloudinary config

--- app/api/create-listing/route.ts
+++ app/api/create-listing/route.ts
@@ -5,6 +5,28 @@ import { randomUUID } from "crypto";
 import { CreateListingData } from "@/app/components/modals/RentModal";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import prisma from "@/app/libs/prismadb";
+import axios from "axios";
+import cloudinary from "cloudinary";
+
+// const uploadImage = async (imagePath) => {
+//   // Use the uploaded file's name as the asset's public ID and
+//   // allow overwriting the asset with new versions
+//   const options = {
+//     use_filename: true,
+//     unique_filename: false,
+//     overwrite: true,
+//     secure: true,
+//   };
+
+//   try {
+//     // Upload the image
+//     const result = await cloudinary.v2.uploader.fi(imagePath, options);
+//     console.log(result);
+//     return result.public_id;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
 
 export async function POST(request: NextRequest) {
   try {
@@ -13,7 +35,6 @@ export async function POST(request: NextRequest) {
     const data = formData.get("data") as unknown as string;
     const parsedData: CreateListingData = JSON.parse(data);
     const currnetUser = await getCurrentUser();
-
     if (!file) {
       return NextResponse.json("Image not exist", { status: 401 });
     }
@@ -26,12 +47,27 @@ export async function POST(request: NextRequest) {
       return NextResponse.json("Authorization required", { status: 401 });
     }
 
-    const bytes = await file.arrayBuffer();
-    const buffer = Buffer.from(bytes);
+    // const bytes = await file.arrayBuffer();
+    // const buffer = Buffer.from(bytes);
 
-    const shortPath = `/images/listings/${randomUUID()}-${file.name}`;
-    const path = `${process.cwd()}/public${shortPath}`;
-    await writeFile(path, buffer);
+    // const shortPath = `/images/listings/${randomUUID()}-${file.name}`;
+    // const path = `${process.cwd()}/public${shortPath}`;
+    // await writeFile(path, buffer);
+
+    const imageFormData = new FormData();
+    imageFormData.set("file", file);
+    imageFormData.append(
+      "api_key",
+      process.env.CLOUDINARY_CLOUD_API_KEY as string
+    );
+
+    imageFormData.append("upload_preset", "likq3xc2");
+
+    const res = await axios.request<{ url: string }>({
+      url: `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/image/upload`,
+      method: "POST",
+      data: imageFormData,
+    });
 
     await prisma.listing.create({
       data: {
@@ -40,7 +76,7 @@ export async function POST(request: NextRequest) {
         bathCount: Number(parsedData.bathroomCount),
         description: parsedData.description,
         guestCount: Number(parsedData.guestCount),
-        imageSrc: shortPath,
+        imageSrc: res.data.url,
         price: Number(parsedData.price),
         roomCount: Number(parsedData.roomCount),
         locationValue: parsedData.location?.value || "",
@@ -50,6 +86,7 @@ export async function POST(request: NextRequest) {
 
     return NextResponse.json("Listing succesfully created");
   } catch (error) {
+    console.log(error);
     return NextResponse.json("Internal server error", { status: 500 });
   }
 }
